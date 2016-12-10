2a5f522331745a2097a78bee882bbff33973d79d
cyeagy <cyeagy@gmail.com>
2016-12-09 17:45:37 -0800

    path slash fix. async checkbox. version in dialog

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-IntelliJ IDEA plugin for generating JAX-RS resource source files.
+##IntelliJ IDEA plugin for generating JAX-RS resource source files
+
+Install jaxrs-client-gen-idea-plugin.zip file manually from the settings plugin section.
+
+####Usage: 
+  - Click "Tools" from the Menu and select "Generate JAX-RS Client"
+  - Select the module the resource is located in.
+  - Enter full classname of resource (i.e. com.foo.ExResource)
+  - Optionally select asynchronous clients.
+  
+It is required to specify the module as the Resource class could exist in many modules, possibly with different versions and forms.
+
+Output will be written to directory jaxrs-client-generated-source/ in the selected module.
+
+---
+I'd like this plugin to function off right-clicking the file desired and adding a action to the context menu. Alas, IDEA documentation blows. Maybe next time.
--- src/io/github/yeagy/jaxrs/GenerateClientAction.java
+++ src/io/github/yeagy/jaxrs/GenerateClientAction.java
@@ -13,10 +13,10 @@ import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.VerticalFlowLayout;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.ws.rs.Path;
+import java.awt.*;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,6 +26,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * id like to generate whole packages of clients in a single call,
+ * but i need to find a better way than to blindly go through the module dependencies trying to string match paths
+ * one possibility is to have a dropdown of dependency jars and let the user specify where to look...
+ */
 public class GenerateClientAction extends AnAction {
     private static final Logger LOG = Logger.getInstance(GenerateClientAction.class);
 
@@ -34,11 +39,11 @@ public class GenerateClientAction extends AnAction {
         new GenerateClientDialog(anActionEvent.getProject()).show();
     }
 
-    private void generateModuleClients(Project project, String moduleName, String className) {
+    private void generateModuleClient(Project project, String moduleName, String className, boolean async) {
         Set<URL> urls = new HashSet<URL>();
         Module module = ModuleManager.getInstance(project).findModuleByName(moduleName);
         extractUrlsFromModule(project, module, urls);
-        generateClients(module, className, urls);
+        generateClient(module, className, urls, async);
     }
 
     private void extractUrlsFromModule(Project project, Module module, Set<URL> urls) {
@@ -67,7 +72,7 @@ public class GenerateClientAction extends AnAction {
     }
 
     private void extractJarUrlFromLibraryRoot(VirtualFile root, Set<URL> urls) {
-        if(root.getPath().endsWith(".jar!/")) {
+        if (root.getPath().endsWith(".jar!/")) {
             File jarFile = new File(root.getPath().substring(0, root.getPath().length() - 2));
             try {
                 urls.add(jarFile.toURI().toURL());
@@ -77,13 +82,13 @@ public class GenerateClientAction extends AnAction {
         }
     }
 
-    private void generateClients(Module module, String className, Set<URL> urls) {
+    private void generateClient(Module module, String className, Set<URL> urls, boolean async) {
         try {
             URLClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[urls.size()]), this.getClass().getClassLoader());
             Class<?> klass = classLoader.loadClass(className);
             if (klass.getAnnotation(Path.class) != null) {
                 VirtualFile moduleRoot = ModuleRootManager.getInstance(module).getContentRoots()[0];
-                new ClientGenerator(false).generate(klass, new File(moduleRoot.getPath(), "jaxrs-client-generated-source"));
+                new ClientGenerator(async).generate(klass, new File(moduleRoot.getPath(), "jaxrs-client-generated-source"));
             }
         } catch (Exception e) {
             LOG.error("could not generate client for class " + className, e);
@@ -94,6 +99,7 @@ public class GenerateClientAction extends AnAction {
         private final Project project;
         private final JTextField targetInput = new JTextField();
         private final ComboBox moduleDropdown;
+        private final Checkbox asyncBox = new Checkbox("Async clients?");
 
         GenerateClientDialog(Project project) {
             super(project);
@@ -108,7 +114,7 @@ public class GenerateClientAction extends AnAction {
             List<String> names = new ArrayList<String>();
             for (Module module : modules) {
                 //todo find a better way to filter out unwanted modules
-                if(!module.getName().endsWith("_test")) {
+                if (!module.getName().endsWith("_test")) {
                     VirtualFile[] libraryRoots = LibraryUtil.getLibraryRoots(new Module[]{module}, false, false);
                     if (libraryRoots.length > 0) {
                         names.add(module.getName());
@@ -118,21 +124,22 @@ public class GenerateClientAction extends AnAction {
             return new ComboBox(names.toArray(), -1);
         }
 
-        @Nullable
         @Override
         protected JComponent createCenterPanel() {
             JPanel panel = new JPanel(new VerticalFlowLayout());
+            panel.add(new JLabel("Generate JAX-RS Client v0.2.1"));
             panel.add(new JLabel("Select Module"));
             panel.add(moduleDropdown);
             panel.add(new JLabel("Full Class Name"));
             panel.add(targetInput);
+            panel.add(asyncBox);
             return panel;
         }
 
         @Override
         protected void doOKAction() {
             String moduleName = (String) moduleDropdown.getSelectedItem();
-            generateModuleClients(project, moduleName, targetInput.getText());
+            generateModuleClient(project, moduleName, targetInput.getText(), asyncBox.getState());
             super.doOKAction();
         }
     }
